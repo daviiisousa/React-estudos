3400c9bd06f09995eaecf0ad83f912eb6ed51f19
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-07-09 20:33:05 -0300

    organizando

---
+++ Aulas/aula001/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/navbar'
+import Hello from './components/Hello';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hello />
+    </>
+  );
+};
+
+export default App;
