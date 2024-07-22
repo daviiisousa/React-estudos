b8ecd03d51acff3790570efff5b440c02e43cfee
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-07-21 22:34:19 -0300

    lista

--- listaDeTarefas/src/components/Formulario/index.tsx
+++ listaDeTarefas/src/components/Formulario/index.tsx
@@ -1,14 +1,62 @@
+import { FormEvent, useEffect, useState } from 'react'
 import './formulario.css'
 
+interface Tarefas{
+    title: string;
+    done: boolean;
+    id: number;
+}
+
 export const Formulario = () =>{
+    const [tarefa, setTarefa] = useState(' ')
+    const [tarefas, setTarefas] = useState([] as Tarefas[]) 
+
+    function QuandoFormRecarregar(event: FormEvent){
+        event.preventDefault()
+
+        const novaTarefa = [
+            ...tarefas,
+            { title: tarefa, id: new Date().getTime(), done: false}
+        ]
+
+        setTarefas(novaTarefa)
+
+        localStorage.setItem('tasks', JSON.stringify(novaTarefa))
+    }
+    
+    useEffect(() =>{
+        const tarefaNoLocalStorage = localStorage.getItem('tasks')
+
+        if(tarefaNoLocalStorage){
+            setTarefas(JSON.parse(tarefaNoLocalStorage))
+        }
+    }, [])
+
+
     return(
         <>
             <h2>Digite suas atividades</h2>
             <label htmlFor="tarefas">Tasks 📑</label>
-            <form onSubmit={(event) => event.preventDefault() }>
-                <input type="text" id='tarefas' placeholder="Digite sua tarefa" />
+            <form onSubmit={QuandoFormRecarregar}>
+                <input 
+                type="text" 
+                id='tarefas'
+                placeholder="Digite sua tarefa"
+                onChange={(e) => {setTarefa(e.target.value)}}
+                value={tarefa}
+                />
                 <button>Enviar</button>
             </form>
+            <ul>
+                {tarefas.map(items =>{
+                    return(
+                    <li key={items.id}>
+                        <input type="checkbox" name="inputTarefa" id={`TaskId ${items.id}`} />
+                        <label htmlFor={`TaskId ${items.id}`}>{items.title}</label>
+                    </li>
+                    )
+                })}
+             </ul>
         </>
     )
 }
---
+++ listaDeTarefas/src/components/Tarefas/index.tsx
@@ -0,0 +1,13 @@
+export const Tarefas = () =>{
+    return(
+        <>
+            <ul>
+                <li>
+                    <input className="tarefaInput" type='checkbox'/>
+                    <span >tarefa 1</span>
+                    <button className="remover">remover</button>
+                </li>
+            </ul>
+        </>
+    )
+}
