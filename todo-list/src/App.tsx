01e928f51d0be55181e1a037e90cfd7968325be5
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-08-17 21:25:17 -0300

    começei meu projeto todo list e ja criei o hedear

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,11 @@
+import { Headear } from "./components/Headear/headear"
+
+function App() {
+  return (
+    <>
+     <Headear />
+    </>
+  )
+}
+
+export default App
---
+++ todo-list/src/components/Headear/headear.tsx
@@ -0,0 +1,20 @@
+import '../../index.css';
+
+export const Headear = () =>{
+    return(
+        <>
+            <header className='bg-blue-950 p-5'>
+                <nav className='mx-10 flex justify-between items-center'>
+                    <div className='flex items-center gap-2'>
+                        <h1 className="text-white text-4xl">Organaice</h1>
+                        <span className="material-symbols-outlined text-white h-4">receipt_long</span>
+                    </div>
+                    <ul className='flex justify-between text-gray-400 gap-3'>
+                        <li>Home</li>
+                        <li>Redes Sociais</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
