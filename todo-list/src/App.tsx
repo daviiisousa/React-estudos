be553b978bf354521a1e83e5102842651cbf8f44
daviiisousa <sousadavi248@gmail.com>
2024-08-30 11:03:14 -0300

    add react royter dom

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,16 +1,22 @@
-
-import { Form } from "./components/Form/form"
-import { Headear } from "./components/Headear/headear"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Form } from "./components/Form/form";
+import { Headear } from "./components/Headear/headear";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
-     <Headear />
-      <main>
-        <Form />
-      </main>
+      <BrowserRouter>
+        <Headear />
+        <main>
+          <Routes>
+            <Route path="/" element={<Form />}></Route>
+            <Route path="/Home" element={<Home />}></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- todo-list/src/components/Headear/headear.tsx
+++ todo-list/src/components/Headear/headear.tsx
@@ -1,16 +1,31 @@
-import '../../index.css';
+import { NavLink } from "react-router-dom";
+import "../../index.css";
 
-export const Headear = () =>{
-    return(
-        <>
-            <header className='bg-blue-950 p-7'>
-                <nav className='mx-10 flex justify-between items-center'>
-                    <div className='flex items-center gap-2'>
-                        <h1 className="text-white text-4xl font-bold">Organice</h1>
-                        <span className="material-symbols-outlined text-white h-4">receipt_long</span>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
-}
+export const Headear = () => {
+  return (
+    <>
+      <header className="bg-blue-950 p-7">
+        <nav className="mx-10 flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <h1 className="text-white text-4xl font-bold">
+                <NavLink to={"/"}>Organice</NavLink>
+            </h1>
+            <span className="material-symbols-outlined text-white h-4">
+              receipt_long
+            </span>
+          </div>
+          <div>
+            <ul className="flex gap-5">
+              <li className="text-white text-xl">
+                <NavLink to={"/"}>Form</NavLink>
+              </li>
+              <li className="text-white text-xl">
+                <NavLink to={"/Home"}>Home</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
